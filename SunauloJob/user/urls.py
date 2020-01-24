bbee9a4cf4df95d2e6077a2ac488480bcb655c35
rhythum12 <52555168+rhythum12@users.noreply.github.com>
2020-01-24 17:21:57 +0545

    added signup and logout

---
+++ SunauloJob/user/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('signup/', view_signup_user),
+]
--- SunauloJob/user/views.py
+++ SunauloJob/user/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from .models import *
+
+
 
 # Create your views here.
+
+def view_signup_user(request):
+    if request.method=="GET":
+        return render(request,'registration/signup.html')
+
+    else:
+        user = User.objects.create_user(username=request.POST['input_username'],password=request.POST['input_password'],email=request.POST['input_email'])
+        user.save()    
+        return HttpResponse("Registration Successful")
+
+def logout_user(request):
+    logout(request)  
+    return redirect ("/user/login")
