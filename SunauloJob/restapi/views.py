d7e2e4551f60322633f75a1229eb02186e790c52
rhythum12 <52555168+rhythum12@users.noreply.github.com>
2020-02-07 16:01:47 +0545

    added restapi GetandPost

--- SunauloJob/restapi/views.py
+++ SunauloJob/restapi/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from jobapp.models import Company
+import json
 
 # Create your views here.
+@csrf_exempt
+def view_get_post_company(request):
+    print("What's the request => ",request.method)
+    if request.method=="GET":
+        companies = Company.objects.all()
+        print("Queryset objects => ",companies)
+        list_of_companies = list(companies.values("companyName","companyAddress","companyContactNo"))
+        print("List of Company objects => ",list_of_companies)
+        dictionary_name = {
+            "companies": list_of_companies
+        }
+        return JsonResponse(dictionary_name)
+
+    elif request.method == "POST":
+        print("Request body content => ",request.body)
+        print("Request body type => ",type(request.body))
+        python_dictionary_object = json.loads(request.body)
+        print("Python dictionary contents =>",python_dictionary_object)
+        print("Python dictionary type =>",type(python_dictionary_object))
+        print(python_dictionary_object['companyName'])
+        print(python_dictionary_object['companyAddress'])
+        print(python_dictionary_object['companyContactNo'])
+        Company.objects.create(companyName=python_dictionary_object['companyName'],companyAddress=python_dictionary_object['companyAddress'],companyContactNo=python_dictionary_object['companyContactNo'])
+        return JsonResponse({
+            "message": "Successfully posted!!"
+        })
+    else:
+        return HttpResponse("Other Http verbs testing")
